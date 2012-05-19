7d210892140497b6c8ec7843a0c4124de5b8a223
Scott Fleckenstein <nullstyle@gmail.com>
2012-05-18 20:34:34 -0700

    Fix haversine function

--- lib/jeeyo/location.rb
+++ lib/jeeyo/location.rb
@@ -9,16 +9,20 @@ module Jeeyo
     
     
     def distance(other)
-      dlat = (self.lat - other.lat)
-      dlon = (self.lon - other.lon)
       
+      dlat = (other.lat - self.lat) >> "radians"
+      dlon = (other.lon - self.lon) >> "radians"
+
       a = (Math.sin(dlat/2) * Math.sin(dlat/2)) +
-          (Math.cos(self.lat) * Math.cos(other.lat) * Math.sin(dlon/2) * (Math.sin(dlon/2)))
+          (Math.cos(self.lat) * Math.cos(other.lat) * 
+          (Math.sin(dlon/2) * Math.sin(dlon/2)))
       
       angular_distance = 2 * Math.atan2( Math.sqrt(a), Math.sqrt(1-a))
       
       EARTH_KILOMETERS_PER_RADIAN * Unit(angular_distance, "radians")
     end
+    
+    private
     
   end
 end
