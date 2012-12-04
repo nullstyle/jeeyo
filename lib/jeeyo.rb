fd61be0a9f911979c23d42796eda01379c442798
Scott Fleckenstein <nullstyle@gmail.com>
2012-12-04 09:53:12 -0800

    Add inverse constant: rad/km for the earth

--- lib/jeeyo.rb
+++ lib/jeeyo.rb
@@ -5,5 +5,6 @@ module Jeeyo
   autoload :Location, "jeeyo/location"
   
   EARTH_KILOMETERS_PER_RADIAN = Unit.new("6367.5 km/rad")
+  EARTH_RADIANS_PER_KILOMETER = EARTH_KILOMETERS_PER_RADIAN.inverse
   
 end
